7acc90dd64da3b7ea14c9011655897d30e169a64
Miraj Patel <mirajvpatel@gmail.com>
2019-03-23 13:05:22 -0400

    register new face backend

--- HomeSecurityAutomation/app/src/main/java/com/example/homesecurityautomation/RegisterNewFace.java
+++ HomeSecurityAutomation/app/src/main/java/com/example/homesecurityautomation/RegisterNewFace.java
@@ -1,13 +1,44 @@
 package com.example.homesecurityautomation;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegisterNewFace extends AppCompatActivity {
-
+    private Button addFace, camButton, back, retakeButton;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_new_face);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(this);
     }
 }
+    public void onClick(View view)
+    {
+        if(view == back)
+        {
+            finish();
+            startActivity(new Intent(this, MainControlActivity.class));
+        }
+    }
