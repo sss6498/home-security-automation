4c5bc6024a1d9453be9b2d3351f6bd7081a1c697
Miraj Patel <mirajvpatel@gmail.com>
2019-03-22 23:06:05 -0400

    user settings page

---
+++ HomeSecurityAutomation/app/src/main/java/com/example/homesecurityautomation/UserSettings.java
@@ -0,0 +1,38 @@
+package com.example.homesecurityautomation;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
+public class UserSettings extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_settings);
+    }
+
+    public void onClick(View view)
+    {
+
+    }
+}
