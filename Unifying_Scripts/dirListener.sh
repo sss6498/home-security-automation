ceed1a623ee04c7eae1a6f65330d1781e4b0bb76
Abhishek Kondila <abhishekkonidela@gmail.com>
2019-05-07 16:53:54 -0400

    Update dirListener.sh

--- Unifying_Scripts/dirListener.sh
+++ Unifying_Scripts/dirListener.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-DIR="Test_Folder"
-DIR_Dest='/Users/akondila/Documents/Destination_Folder'
+DIR="/home/pi/Arduino/ArduCAM_OV562-master/data/capture/"
+#DIR="./Test_Folder"
+#DIR1="~/home/pi/Desktop/uploadToFirebase/Test_Folder/"
+EXT=".jpg"
+COMP=15
 
+export GOOGLE_APPLICATION_CREDENTIALS='HomeAutomationCreds.json'
 
-inotifywait -m DIR -e create -e moved_to |
+inotifywait -m $DIR -e create -e moved_to |
     while read path action file; do 
-    echo $file
-    echo $action
-    echo $path
-#    mv $file $DIR_Dest
-#    ls $DIR_Dest
-    python3 UploadToFirebase.py $file
+    echo ${#file}
+    if [ ${#file} -lt $COMP ];
+    then
+	    echo $file
+	    file=${file:0:9}$EXT
+	    echo $file
+	    echo $action
+	    echo $path
+	    python3 UploadToFirebase.py "$file"
+	    mv $DIR$file ~/Desktop/DEMO/images
+	    
+	    cd ../
+	    result=$(./facialrec.sh $file)
+	    cd uploadToFirebase
+	    python3 FaceRegBooleanUpload.py $result
+   	    echo "Done"
+    fi
+
   done
